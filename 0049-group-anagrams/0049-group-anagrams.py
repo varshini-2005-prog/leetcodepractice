3167181744fad7c8fe4d429c87c2bd3cf8fac4af
VARSHINI R <varshini3789@gmail.com>
2026-04-13 21:43:43 +0530

    Time: 11 ms (84.55%), Space: 22 MB (70.91%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs):
+
+        d = {}
+
+        for word in strs:
+            key="".join(sorted(word))
+            if key not in d:
+                d[key]=[]
+
+            d[key].append(word)
+        return list(d.values())
