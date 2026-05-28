b88f778e28e13fc6a21eb5873be968112de5fc14
VARSHINI R <varshini3789@gmail.com>
2026-05-28 14:30:42 +0530

    Time: 6076 ms (5%), Space: 19.4 MB (53.64%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,27 +1,9 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        need = Counter(s1)
-
-        left = 0
-        count = len(s1)
-
-        for i in range(len(s2)):
-
-            if need[s2[i]] > 0:
-                count -= 1
-
-            need[s2[i]] -= 1
-
-            if i - left + 1 > len(s1):
-
-                need[s2[left]] += 1
-
-                if need[s2[left]] > 0:
-                    count += 1
-
-                left += 1
-
-            if count == 0:
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        k=len(s1)
+        for i in range(len(s2)-k+1):
+            sub=s2[i:i+k]
+            if sorted(sub)==sorted(s1):
                 return True
-
+                break
         return False
